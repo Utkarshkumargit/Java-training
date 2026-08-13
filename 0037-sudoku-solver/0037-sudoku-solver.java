2dd52db05ffe0ee45b8bcc9dc12c791175c6246b
Utkarshkumargit <utkarshkumar5005@gmail.com>
2026-08-13 14:36:24 +0530

    Time: 228 ms (58.98%), Space: 42.5 MB (94.3%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,63 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        //find an empty cell
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    //try 1 to 9 
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            //recurse
+                            if (solve(board)) {
+                                return true;
+                            }
+                            //undo
+                            board[row][col] = '.';
+                        }
+                    }
+                    //no number matched
+                    return false;
+                }
+            }
+        }
+
+        //no empty cell -> sudoko solve
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        //check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) {
+                return false;
+            }
+        }
+
+        //check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        //check 3*3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
